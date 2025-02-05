3b6e7e99c0731ce6ad95097f2e04d6ed0e47a82d
estaub <ed@semanterra.org>
2025-02-04 21:04:23 -0500

    Update dependencies. Big refactor of process_station_csv. Finish stations_attr_use.

--- src/ghcnd/process_station_csv.py
+++ src/ghcnd/process_station_csv.py
@@ -5,6 +5,9 @@ import tarfile
 import polars as pl
 import io
 import warnings
+
+from line_profiler_pycharm import profile
+
 warnings.filterwarnings("ignore", message="Polars found a filename")
 from make_daily_summary_schema import make_schema
 from read_daily_summary_gz import read_daily_summary_gz
@@ -22,10 +25,12 @@ column_enum = pl.Enum(schema.keys())
 
 station_flat_schema = {
     'STATION':pl.String,
-    'N_COLUMNS': pl.Int32,
+    'N_OBVALUES': pl.Int32,
     'N_ROWS': pl.Int32,
     'DATE_MIN':pl.Date,
-    'DATE_MAX':pl.Date
+    'DATE_MAX':pl.Date,
+    'N_LOCATIONS': pl.Int32,
+    'N_NAMES': pl.Int32
 }
 
 station_describe_schema = {
@@ -48,120 +53,133 @@ station_attr_use_schema = {
 }
 
 def process_station_csv(tar_info, buff, output_dict):
-    header_string = buff.peek(1000).split(b'\n')[0].decode("utf-8")
-    header = list(map(lambda s: s[1:-1], header_string.split(',')))
-    filtered_schema = {key: val for key, val in schema.items() if key in header}
+
+    def read_station_csv(buff):
+        header_string = buff.peek(1000).split(b'\n')[0].decode("utf-8")
+        header = list(map(lambda s: s[1:-1], header_string.split(',')))
+        filtered_schema = {key: val for key, val in schema.items() if key in header}
+
+        station_df = pl.read_csv(
+            buff,
+            has_header=True,
+            schema=filtered_schema,
+        )
+        return station_df, filtered_schema
+
+
+    def split_attributes(station_df):
+        counts_df = station_df.count()
+        attr_selects = []
+        split_struct_cols = []
+        for attributes_col in attribute_cols :
+            if counts_df.item(0,attributes_col) > 0:
+                obvalue =  attributes_col.removesuffix('_ATTRIBUTES')
+                split_struct_col = obvalue+'_SPLIT_ATTRS'
+                split_struct_cols.append(split_struct_col)
+                attr_selects.append(
+                    pl.col(attributes_col)
+                        .str.split_exact(",", 3)
+                        .struct.rename_fields([obvalue+"_MEASURE", obvalue+"_QUALITY", obvalue+"_SOURCE"])
+                        .alias(split_struct_col)
+                )
+
+        unnested_df = station_df.select(attr_selects).unnest(split_struct_cols)
+        nulled_df = unnested_df.with_columns(
+            pl.when(pl.col(pl.String).str.len_chars() == 0)
+            .then(None)
+            .otherwise(pl.col(pl.String))
+            .name.keep()
+        )
+        return station_df.with_columns(nulled_df)
+
+
+    def record_flat_data(station_df, output_dict):
+        n_names = station_df.n_unique(subset=pl.col('NAME'))
+        n_locations = station_df.n_unique(subset=pl.col(['LATITUDE','LONGITUDE','ELEVATION']))
+
+        station_flat_data = {
+            'STATION':station,
+            'N_OBVALUES': len(station_obvalues),
+            'N_ROWS': station_df.height,
+            'DATE_MIN':min_df.item(0,'DATE'),
+            'DATE_MAX':max_df.item(0,'DATE'),
+            'N_LOCATIONS': n_locations,
+            'N_NAMES': n_names,
+        }
+        station_flat_df = pl.DataFrame( station_flat_data, schema=station_flat_schema)
+        output_dict['stations_flat'].vstack(station_flat_df, in_place=True)
+
+
+    def record_describe_data(station_df, output_dict):
+
+        counts = station_df.count().row(0)
+        null_counts = station_df.null_count().row(0)
+
+        def clean_nums(col):
+            ret = list(col)
+            for i, data in enumerate(col):
+                if not (type(data) == int or type(data) == float):
+                    ret[i] = None
+            return ret
+
+        station_describe_data = {
+            'STATION': [station]*len(station_df.columns),
+            'COLUMN':station_df.columns,
+            'COUNT':counts,
+            'MAX_FLOAT': clean_nums(maxs),
+            'MIN_FLOAT': clean_nums(mins),
+            'NULL_COUNT': null_counts,
+        }
+
+        station_describe_df = pl.DataFrame( station_describe_data, schema=station_describe_schema)
+        output_dict['stations_describe'].vstack(station_describe_df, in_place=True)
+
+
+    def record_attr_use_data(station_df, output_dict):
+        for attr in ['MEASURE', 'QUALITY', 'SOURCE']:
+            for obvalue in station_obvalues:
+                col = obvalue + '_' + attr
+                value_count_df = (station_df[col].value_counts()
+                     .rename({col:'VALUE', 'count':'COUNT'})
+                     .filter(pl.col('VALUE').is_not_null()))
+                if value_count_df.height:
+                    value_count_df.insert_column(0, pl.lit(station).alias('STATION'))
+                    value_count_df.insert_column(1, pl.lit(obvalue).cast(column_enum).alias('OBVALUE'))
+                    value_count_df.insert_column(2, pl.lit(attr).cast(attr_type_enum).alias('ATTR'))
+
+                    if output_dict['stations_attr_use'].height:
+                        output_dict['stations_attr_use'].vstack(value_count_df, in_place=True)
+                    else:
+                        output_dict['stations_attr_use'] = value_count_df
+
+    station_df, filtered_schema = read_station_csv(buff)
+
     attribute_cols = list(filter(lambda col: col.endswith('_ATTRIBUTES'), filtered_schema.keys()))
     station_obvalues = list(map(lambda col: col.removesuffix('_ATTRIBUTES'), attribute_cols))
-    station_df = pl.read_csv(
-        buff,
-        has_header=True,
-        schema=filtered_schema,
-    )
 
-    counts_df = station_df.count()
-    attr_selects = []
-    split_struct_cols = []
-    for attributes_col in attribute_cols :
-        if counts_df.item(0,attributes_col) > 0:
-            obvalue =  attributes_col.removesuffix('_ATTRIBUTES')
-            split_struct_col = obvalue+'_SPLIT_ATTRS'
-            split_struct_cols.append(split_struct_col)
-            attr_selects.append(
-                pl.col(attributes_col)
-                    .str.split_exact(",", 3)
-                    .struct.rename_fields([obvalue+"_MEASURE", obvalue+"_QUALITY", obvalue+"_SOURCE"])
-                    .alias(split_struct_col)
-            )
-
-    unnested_df = station_df.select(attr_selects).unnest(split_struct_cols)
-    nulled_df = unnested_df.with_columns(
-        pl.when(pl.col(pl.String).str.len_chars() == 0)
-        .then(None)
-        .otherwise(pl.col(pl.String))
-        .name.keep()
-    )
-    station_df = station_df.with_columns(nulled_df)
+    station_df = split_attributes(station_df)
 
     station = tar_info.name[:11]
-
-    counts = station_df.count().row(0)
     max_df=station_df.max()
     maxs = max_df.row(0)
     min_df = station_df.min()
     mins = min_df.row(0)
-    null_counts = station_df.null_count().row(0)
 
-    station_flat_data = {
-        'STATION':station,
-        'N_COLUMNS': station_df.width,
-        'N_ROWS': station_df.height,
-        'DATE_MIN':min_df.item(0,'DATE'),
-        'DATE_MAX':max_df.item(0,'DATE'),
+    record_flat_data(station_df, output_dict)
+    record_describe_data(station_df, output_dict)
+    record_attr_use_data(station_df, output_dict)
+    print(output_dict)
+
+def main():
+    output_dicts = {
+        'stations_flat': pl.DataFrame([], schema=station_flat_schema),
+        'stations_describe': pl.DataFrame([], schema=station_describe_schema, orient='row'),
+        'stations_attr_use':  pl.DataFrame([], schema=station_attr_use_schema, orient='row'),
     }
-    station_flat_df = pl.DataFrame( station_flat_data, schema=station_flat_schema)
-    output_dict['stations_flat'].vstack(station_flat_df, in_place=True)
+    read_daily_summary_gz(daily_summary_path, process_station_csv, output_dicts, max_stations=1002)
 
-    def clean_nums(col):
-        ret = list(col)
-        for i, data in enumerate(col):
-            if not (type(data) == int or type(data) == float):
-                ret[i] = None
-        return ret
+main()
 
-    station_describe_data = {
-        'STATION': [station]*len(station_df.columns),
-        'COLUMN':station_df.columns,
-        'COUNT':counts,
-        'MAX_FLOAT': clean_nums(maxs),
-        'MIN_FLOAT': clean_nums(mins),
-        'NULL_COUNT': null_counts,
-    }
-
-    station_describe_df = pl.DataFrame( station_describe_data, schema=station_describe_schema)
-    output_dict['stations_describe'].vstack(station_describe_df, in_place=True)
-
-    for attr in ['MEASURE', 'QUALITY', 'SOURCE']:
-        for obvalue in station_obvalues:
-            col = obvalue + '_' + attr
-            x = (station_df[col].value_counts()
-                 .rename({col:'VALUE', 'count':'COUNT'})
-                 .filter(pl.col('VALUE').is_not_null()))
-            if x.height:
-                x.insert_column(0, pl.lit(station).alias('STATION'))
-                x.insert_column(1, pl.lit(obvalue).alias('OBVALUE'))
-                x.insert_column(2, pl.lit(attr).alias('ATTR'))
-
-                output_dict['stations_attr_use'].vstack(x, in_place=True)
-'''
-    # TODO
-    station_attr_use_data = {
-        'STATION': [station]*(len(station_obvalues)*3),
-        'OBVALUE':pl.String, # TODO 3 consecutive copies of obvalue name for each obvalue
-        'ATTR': ['MEASURE', 'QUALITY', 'SOURCE'] * len(station_obvalues),
-        'VALUE':
-        'COUNT':
-    }
-    station_attr_use_df = pl.DataFrame(station_attr_use_data, schema=station_attr_use_schema)
-    output_dict['stations_attr_use'].vstack(station_attr_use_df, in_place=True)
-'''
-    # print(description.describe())
-    # print(description)
-    # print(station_df.glimpse())
-    # print(station_df.count())
-    # print(station_df.item(0,'DATE'))                        # date of first observation
-    # print(station_df.item(station_df.shape[0]-1, 'DATE'))   # date of last observation
-    # print(station_df.drop_nulls('PRCP').shape[0] if 'PRCP' in station_df.columns else 0 ) # observations with non-null precip
-
-
-output_dicts = {
-    'stations_flat': pl.DataFrame([], schema=station_flat_schema),
-    'stations_describe': pl.DataFrame([], schema=station_describe_schema, orient='row'),
-    'stations_attr_use':  pl.DataFrame([], schema=station_attr_use_schema, orient='row'),
-}
-read_daily_summary_gz(daily_summary_path, process_station_csv, output_dicts)
-
-output_dicts['stations_flat'].describe()
 # TODO save dict to disk
 # TODO get aggregate statistics for each column from stations_describe - # of stations for each stat
 # TODO characterize column usage
